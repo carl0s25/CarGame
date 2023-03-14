505fc2343e687c89a12b715ead52ec8f194af181
Carlos Contreras <89582094+carl0s25@users.noreply.github.com>
2023-03-14 09:42:13 -0500

    Corrección en los simbolos

--- CarGame.cpp
+++ CarGame.cpp
@@ -16,10 +16,10 @@ COORD CursorPosition;
 int enemyY[3];
 int enemyX[3];
 int enemyFlag[3];
-char car[4][4] = { ' ','�','�',' ', 
-					'�','�','�','�', 
-					' ','�','�',' ',
-					'�','�','�','�' }; 
+char car[4][4] = {' ','±','±',' ', 
+		 '±','±','±','±', 
+		 ' ' ,'±','±',' ',
+		 '±','±','±','±' }; 
 					
 int carPos = WIN_WIDTH/2;
 int score = 0; 
